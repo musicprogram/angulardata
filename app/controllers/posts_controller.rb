0d7992d71212ed75f7d639ea6cbcaf5694b36c03
JuanH <juan-o-henao@hotmail.es>
2015-01-25 13:57:47 -0500

    devise style

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -12,11 +12,11 @@ class PostsController < ApplicationController
 
 
 	def new
-		@post = Post.new
+		@post = current_user.posts.build
 	end
 
 	def create
-		@post = Post.new(post_params)
+		@post = current_user.posts.build(post_params)
 		if @post.save
 			redirect_to @post
 		else 
