69ad790cb47cbef27275f2cc8eca228f8cce5515
JuanHenao <juan-o-henao@hotmail.es>
2015-02-27 19:52:31 +0000

    diferentes background layouts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
 	before_action :find_post, only: [:show, :edit, :update, :destroy, :upvote, :downvote]
 	before_action :authenticate_user!, except: [:index, :show]
 	def index
+        @home_pagee = true
 		@posts = Post.all.order("created_at DESC")
 	end
 
